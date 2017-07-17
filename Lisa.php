bcf6c0974daaf0bf5a8c482d26692ba78296718f
jacobOrtega <jenku18@gmail.com>
2017-07-17 23:27:54 +0300

    Added lisa.php file

---
+++ Lisa.php
@@ -0,0 +1,40 @@
+<head>
+
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>RIHA XML Varamu</title>
+    <link rel="shortcut icon" href="https://www.ria.ee/extensions/ria_2011/images/favicon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bower_components/select2/dist/css/select2.min.css">
+    <link rel="stylesheet" href="css/bower_components/jquery-ui/themes/base/jquery-ui.min.css">
+    <link rel="stylesheet" href="css/bower_components/datatables/media/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="css/bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css">
+    <link rel="stylesheet" href="css/bower_components/build/app.css">
+
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<div id="wrap">
+    <?php include 'header.html'; ?>
+    <!--div id="headerInclude" /></div-->
+    <main>
+        <div class="add-form">
+            <form>
+                <label for="resource-name">Nimetus eesti keeles</label>
+                <div>
+                    <input id="resource-name" type="text">
+                </div>
+            </form>
+        </div>
+
+    </main>
+    <?php include 'footer.html'; ?>
+    <!--div id="footerInclude" /></div-->
+
+</div>
+</body>
