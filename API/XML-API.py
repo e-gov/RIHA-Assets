ea30238a69a2498d3e57281819ee1ff68050d973
Holger Hansson <holger@hansson@ria.ee>
2017-08-18 10:31:55 +0300

    Added API script

---
+++ API/XML-API.py
@@ -0,0 +1,33 @@
+from bottle import Bottle, run, post, request, response
+from random import randint
+import json, hashlib, datetime, unicodedata, re
+
+def enable_cors(fn):
+	def wrapper(*args, **kwargs):
+		response.headers['Access-Control-Allow-Origin'] = '*'
+		response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+		response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+		if request.method != 'OPTIONS':
+			return fn(*args, **kwargs)
+	
+	return wrapper
+
+app = Bottle()
+
+@app.route("/save", method=['OPTIONS','POST'])
+@enable_cors
+def save_xml():
+	XMLjson = json.loads(json.dumps(request.json))
+	nimetus = XMLjson["Name_est"]
+	nimetus = str(unicodedata.normalize('NFKD', nimetus).encode('ascii', 'ignore'))
+	nimetus = str(re.sub('[^\w\s-]', '', nimetus).strip().lower())
+	nimetus = str(re.sub('[-\s]+', '-', nimetus))
+	timestamp = datetime.datetime.now().strftime('%Y.%m.%d.%H:%M:%S')
+	hash_object = hashlib.sha256((nimetus+timestamp+str(randint(0, 1000))).encode('utf-8'))
+	hex_dig = hash_object.hexdigest()
+	timestamp = timestamp[:10]
+	nimi = "{0}-{1}-{2}".format(timestamp,nimetus,str(hex_dig))
+	file = open(nimi+'.json', 'w+')
+	file.write(json.dumps(XMLjson))
+
+app.run(host='localhost', port=8080)
