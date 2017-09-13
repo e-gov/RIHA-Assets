f84ca6440172eb0e64adc9020950a35454cd3a43
hanneskiivet <hannes.kiivet@ria.ee>
2017-09-13 05:02:05 +0300

    Restructured code (unified header and footer design with RIHA-Browser, removed unused/-useful code) and simplified resource tree. Images and favicons are now sought from /assets directory as in RIHA-Browser

--- index.php
+++ index.php
@@ -1,15 +1,65 @@
-<!DOCTYPE html>
+<?php
+
+$resourcesDirectory = 'resources/';
+$resourcesListFile = $resourcesDirectory . 'resources.json';
+$resourcesFilesListFile = $resourcesDirectory . 'files.js';
+$resourceDescriptionFile = 'resource.json';
+
+$workDir = getcwd().'/';#str_replace('\\','/', getcwd());
+    if (!@file_exists($workDir . $resourcesListFile)){
+    header('Location: 404.html');
+    exit;
+    }
+
+$iti = new RecursiveDirectoryIterator($workDir . $resourcesDirectory);
+$resourceDescriptions = array();
+$recursiveIti = new RecursiveIteratorIterator($iti);
+$files = array();
+    foreach ($recursiveIti as $file) {
+        if (substr($file, -1) != ".." && substr($file, -1) != "." && strpos($file, $resourceDescriptionFile) === false && strpos($file, $resourcesFilesListFile) === false && strpos($file, $resourcesListFile) === false) {
+        $file = substr($file, strpos($file, '/resources'));
+        $files[] = convertFileName($file);
+        }
+    }
+    foreach ($recursiveIti as $file) {
+        if (strpos($file, $resourceDescriptionFile) !== false) {
+        $string = file_get_contents($file);
+        $json_a = json_decode($string, true);
+        $asset = $json_a[0];
+        $dir = convertFileName(dirname(substr($file, strpos($file, '/resources')+11)));
+        $files2 = array();
+            foreach ($files as $file2) {
+                if (strpos($file2, $dir) !== false)
+                $files2[] = $file2;
+            }
+        $asset['Files'] = $files2;
+        array_push($resourceDescriptions, json_encode($asset));
+        }
+    }
+$newResourceListJSON = '[' . implode($resourceDescriptions, ',') . ']';
+$resourceListJSON = file_get_contents($workDir . $resourcesListFile);
+    if($resourceListJSON != $newResourceListJSON)
+        file_put_contents($workDir . $resourcesListFile, $newResourceListJSON, LOCK_EX);
+
+function convertFileName($file) {
+$file = str_replace('\\', '/', $file);
+$file = (substr($file, 0 ,1)=='/' ? substr($file, 1) : $file);
+$file = mb_convert_encoding($file, 'UTF-8', mb_detect_encoding($file, 'auto'));
+return $file;
+}
+
+?><!DOCTYPE html>
 <html lang="et">
 
 <head>
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>RIHA XML varamu</title>
-    <link rel="shortcut icon" href="css/bower_components/images/favicon.png">
+    <title>XML varamu - Riigi infosüsteemi haldussüsteem RIHA</title>
+    <link rel="icon" type="image/png" href="/assets/coat-of-arms-favicon.png">
+    <link rel="apple-touch-icon" href="/assets/apple-touch-coat-of-arms-favicon.png">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/mobileview.css">
     <link rel="stylesheet" href="css/bower_components/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/bower_components/select2/dist/css/select2.min.css">
     <link rel="stylesheet" href="css/bower_components/jquery-ui/themes/base/jquery-ui.min.css">
@@ -17,12 +67,8 @@
     <link rel="stylesheet" href="css/bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css">
     <link rel="stylesheet" href="css/bower_components/build/app.css">
 
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/jquery-3.2.1.js"></script>
-    <script src="css/bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
-    <script src="css/bower_components/datatables/media/js/dataTables.bootstrap4.min.js"></script>
-    <script src="js/Detailview.js"></script>
-    <?php include 'js/Detailview.php'; ?>
+    <link rel="stylesheet" href="css/resources.css">
+    <script src="css/bower_components/jquery/dist/jquery.min.js"></script>
     <script type="text/javascript">
         $(function () {
             $("#headerInclude").load("header.html #header");
@@ -35,44 +81,51 @@
 <body>
 <div id="headerInclude">
 </div>
-<main id="content" class="container" style="background-image: url('css/bower_components/images/bg.png')">
-    <div class="container-fluid my-1">
-        <h1 id="title">RIHA XML varamu</h1>
-        <div class="container">
-
-            <div class="table-responsive">
-                <table id="xml-resources-table" class="table table-striped table-bordered table-hover dt-responsive dataTable " cellspacing="0" width="50%">
-                    <p id="description">XML vara on RIHA registriobjekt, mis sisaldab vara põhiosaks olevat XML
-                    skeemi, seotud dokumente ja metaandmeid. RIHA XML varamus registreeritakse ainult kasutuses olevad
-                    varad.</p>
-                    <thead id="resources-head" >
-
-                    <tr>
-                        <th>Nimi</th>
-                        <th>Staatus</th>
-                        <th>Vara liik</th>
-                    </tr>
+<main class="container" id="content">
+    <header class="pagehead" style="background-image:url(/assets/images/data/intro-bg.png)">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="mb-0 mb-md-4">XML varamu</h1>
+            </div>
+        </div>
+    </header>
+    <section class="col card p-3">
+        <div class="my-1">
+            <p id="description" class="table-view">RIHA XML varamu ülesandeks on kontrollida ja säilitada dokumendiliigi ning metaandmete andmekirjeldusi, mida edastatakse <a href="https://www.ria.ee/ee/dokumendivahetus-dhx.html">asutustevahelise dokumendivahetuslahenduse (DVK/DHX)</a> kaudu. <a href="Abi/XML-varamu.html">Loe lähemalt RIHA abimaterjalidest</a></p>
+            <div>
+                <table cellspacing="0" class="table table-striped table-bordered dataTable table-view" id="xml-resources-table" width="100%">
+                    <thead class="thead-default">
+                        <tr>
+                            <th>
+                                <button class="btn btn-primary btn-sm">Nimi
+                                </button>
+                            </th>
+                            <th>
+                                <button class="btn btn-primary btn-sm">Staatus
+                                </button>
+                            </th>
+                            <th>
+                                <button class="btn btn-primary btn-sm">Vara liik
+                                </button>
+                            </th>
+                        </tr>
                     </thead>
-
                     <tbody id="xml-resources">
                     <script id="main-row-template" type="text/x-custom-template">
                         <tr>
-                            <td><a  class="name_and_version"></a></td>
+                            <td><a class="name_and_version"></a></td>
                             <td class="status"></td>
                             <td class="resource_type"></td>
                         </tr>
                     </script>
-
                     </tbody>
                 </table>
-            </div>
-        </div>
 
-        <div id="detail">
+        <div id="detail" class="detail-view">
             <div class="menubar" style="text-align:center;">
                 <br>
                 <button type="button" class="btn btn-outline-primary btn-sm backButton"
-                        style="float: left; margin-left: 10px;">Tagasi
+                        style="float: left; margin-left: 10px;">Tagasi nimekirja
                 </button>
                 <button type="button" class="btn btn-primary btn-sm dataButton" style="margin-right: 2%;">Üldandmed
                 </button>
@@ -80,81 +133,71 @@
 
                 <br>
                 <hr>
+                <span class="versionbar"></span>
             </div>
-            <table id="detail-table" class="table">
+            <table id="detail-table" class="table content">
                 <tbody id="detail-tbody">
                 <script id="detail-row-template" type="text/x-custom-template">
                     <tr>
-                        <th>Nimetus, eesti keeles:</th>
+                        <td colspan="2"><h2 id="name_est"></h2></td>
                     </tr>
                     <tr>
-                        <td id="name_est"></td>
+                        <td colspan="2">
+                            <div class="form-group">
+                                <label>Omanik:</label>
+                                <p id="owner"></p>
+                            </div>
+                        </td>
                     </tr>
-
                     <tr>
+                        <td colspan="2">
+                            <div class="form-group">
+                                <label>Kirjeldus:</label>
+                                <p id="des_est"></p>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div class="form-group">
+                                <label>Versioon:</label>
+                                <p id="version"></p>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div class="form-group">
+                                <label>Kehtiv:</label>
+                                <p><span id="start_date"></span>...<span id="end_date"></span></p>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div class="form-group">
+                                <label>Vara liik:</label>
+                                <p id="resource_type"></p>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr style="display: none;">
                         <th>Nimetus, inglise keeles:</th>
-                    </tr>
-                    <tr>
                         <td id="name_eng"></td>
                     </tr>
-
-                    <tr>
-                        <th>Versioon:</th>
-                    </tr>
-                    <tr>
-                        <td id="version"></td>
-                    </tr>
-
-                    <tr>
-                        <th>Kehtiv alates:</th>
-                    </tr>
-                    <tr>
-                        <td id="start_date"></td>
-                    </tr>
-
-                    <tr>
-                        <th>Kehtiv kuni:</th>
-                    </tr>
-                    <tr>
-                        <td id="end_date"></td>
-                    </tr>
-
-                    <tr>
-                        <th>Kirjeldus, eesti keeles:</th>
-                    </tr>
-                    <tr>
-                        <td id="des_est"></td>
-                    </tr>
-
-                    <tr>
+                    <tr style="display: none;">
                         <th>Kirjeldus, inglise keeles:</th>
-                    </tr>
-                    <tr>
                         <td id="des_eng"></td>
-                    </tr>
-
-                    <tr>
-                        <th>Vara liik:</th>
-                    </tr>
-                    <tr>
-                        <td id="resource_type"></td>
-                    </tr>
-
-                    <tr>
-                        <th>Haldaja:</th>
-                    </tr>
-                    <tr>
-                        <td id="owner"></td>
                     </tr>
                 </script>
                 </tbody>
             </table>
         </div>
-        <div id="files" style="background-color: white;">
+        <div id="files" style="background-color: white;" class="detail-view">
             <div class="menubar" style="text-align:center;">
                 <br>
                 <button type="button" class="btn btn-outline-primary btn-sm backButton"
-                        style="float: left; margin-left: 10px;">Tagasi
+                        style="float: left; margin-left: 10px;">Tagasi nimekirja
                 </button>
                 <button type="button" class="btn btn-outline-primary btn-sm dataButton" style="margin-left: -9%;">
                     Üldandmed
@@ -164,16 +207,15 @@
                 <hr>
             </div>
             <div id="files-content" style="word-wrap: break-word;">
-
             </div>
 
         </div>
-    </div>
+            </div>
+        </div>
+    </section>
 </main>
-<div id="footerInclude">
+<div id="footerInclude"/>
 </div>
-
-<script src="css/bower_components/jquery/dist/jquery.min.js"></script>
 <script src="css/bower_components/jquery-ui/jquery-ui.min.js"></script>
 <script src="css/bower_components/tether/dist/js/tether.min.js"></script>
 <script src="css/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
@@ -182,6 +224,17 @@
 <script src="css/bower_components/datatables-responsive/js/dataTables.responsive.js"></script>
 <script src="css/bower_components/datatables/media/js/dataTables.bootstrap4.min.js"></script>
 <script src="css/bower_components/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="js/Detailview.js"></script>
+
+<!-- Google Analytics Tracking Code for test.riha.ee -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-66693747-2', 'auto');
+  ga('send', 'pageview');
+</script>
 </body>
 
 </html>
--- js/Detailview.js
+++ js/Detailview.js
@@ -1,11 +1,146 @@
 /**
- * Created by janarp on 03/08/17.
+ * Created by janarp on 03/08/17. Updated by hannesg on 13/09/17
  */
 
-$('#xml-resources-table').DataTable({
-    responsive: {
-        details: {
-            type: 'column'
+$(document).ready(function () {
+    $('.detail-view').hide();
+    var template = $('#main-row-template').html();
+    var tBody = $('#xml-resources');
+    var id = "";
+    $.getJSON('resources/resources.json', function (data) {
+        var resourceName = "";
+        var resourceVersion = "";
+        var usedNames = [];
+        //show only repeating names' latest version
+        data.forEach(function (resource, index) {
+            var newRow = $(template);
+            if (resourceName == resource.Name_est) {
+                if (parseInt(resource.Version, 10) > parseInt(resourceVersion, 10)) {
+                    newRow.find('.name_and_version').text(resource.Name_est).attr("id", index).attr('href', '#'+index);
+                    newRow.find('.status').text(resource.Status);
+                    newRow.find('.resource_type').text(resource.Resource_type);
+                    resourceName = resource.Name_est;
+                    usedNames.push(resourceName);
+                    tBody.append(newRow);
+                }
+            } else {
+                resourceName = resource.Name_est;
+                resourceVersion = resource.Version;
+            }
+            $('#files').hide();
+        });
+        //go through all again, but show only non-duplicates
+        data.forEach(function (resource, index) {
+            var newRow = $(template);
+            if (!usedNames.includes(resource.Name_est)) {
+                newRow.find('.name_and_version').text(resource.Name_est).attr("id", index).attr('href', '#'+index);
+                newRow.find('.status').text(resource.Status);
+                newRow.find('.resource_type').text(resource.Resource_type);
+                tBody.append(newRow);
+            }
+        });
+        $('.name_and_version').click(function (event) {
+            var resource = data[event.target.id];
+            id = resource;
+            var temp = $('#detail-row-template').html();
+            var dtbody = $('#detail-tbody');
+            $('#detail-tbody tr').remove();
+            var currentName = resource.Name_est;
+            var currentVersion = resource.Version;
+            var newRow = $(temp);
+            $('.versionbar').empty();
+            if (usedNames.includes(resource.Name_est)) {
+                data.forEach(function (resource, index) {
+                    if (resource.Name_est == currentName) {
+                        $('.versionbar').append('<button type="button" id="' + index + '" class="btn btn-' + (resource.Version == currentVersion ? '' : 'outline-') + 'primary btn-sm versionButton">V' + resource.Version + '</button>');
+                    }
+                });
+            }
+            $('.versionButton').click(function (event) {
+                $('.versionButton').attr("class", "btn btn-outline-primary btn-sm versionButton");
+                $(this).attr("class", "btn btn-primary btn-sm versionButton");
+                resource = data[event.target.id];
+                id = resource;
+                updateFields(newRow, resource);
+                newRow.find('#field').text(resource.Field);
+                dtbody.append(newRow);
+            });
+            updateFields(newRow, resource);
+            dtbody.append(newRow);
+            $('.table-view').hide();
+            $('#detail').show();
+            $('#files').hide();
+        });
+        $('.fileButton').on('click', function (event) {
+            $('.detail-view').hide();
+            $('#files-content').html(getListOfFileNames(id.Name_est + ' (' + id.Version + ')', id.Files));
+            $('#files').show();
+        });
+        $('.name_and_version').css("color", "#023cc1").hover(function () {
+            $(this).css("cursor", 'pointer');
+            $(this).css("color", "black");
+        }, function () {
+            $(this).css("color", "#023cc1");
+        });
+        if (!(location.hash==='')) {
+        var pageId = location.hash.substr(1,1);
+        $('#'+pageId).trigger('click');
         }
-    }
+    });
+    $('.backButton').on('click', function (event) {
+        $('.table-view').show();
+        $('.detail-view').hide();
+        location.hash = '';
+    });
+    $('.dataButton').on('click', function (event) {
+        $('.table-view').hide();
+        $('.detail-view').show();
+        $('#files').hide();
+    });
+    $(window).on('hashchange', function() {
+        var pageId = location.hash.substr(1,1);
+        $('#'+pageId).trigger('click');
+    });
+
+/*    $('#xml-resources-table').DataTable({
+        responsive: {
+            details: {
+                type: 'column'
+            }
+        },
+        "paging": false,
+        "searching": false,
+        "language": {
+          "emptyTable": "Ei leitud ühtegi XML vara",
+          "info": "",
+          "infoEmpty": ""
+        }
+    }); */
 });
+
+function findFileName(filepath) {
+return filepath.substring(filepath.indexOf('/files/') + ('/files/').length);
+}
+
+function getListOfFileNames(name, files) {
+var t = '<table class="content"><tbody><tr><td><h2>' + name + '</h2></td></tr></tbody></table><ul>';
+    for (var index = 0; index < files.length; index++) {
+    t += '<li><a href="' + '.' + files[index] + '">' + findFileName(files[index]) + '</a></li>';
+    }
+t += '</ul>';
+return t;
+}
+
+function updateFields(newRow, resource) {
+newRow.find('#name_est').text(resource.Name_est);
+newRow.find('#name_eng').text(resource.Name_eng);
+newRow.find('#version').text(resource.Version);
+newRow.find('#start_date').text(resource.Start_date);
+newRow.find('#end_date').text(resource.End_date);
+newRow.find('#des_est').text(resource.Description_est);
+newRow.find('#des_eng').text(resource.Description_eng);
+newRow.find('#stored').text(resource.Stored_until);
+newRow.find('#resource_type').text(resource.Resource_type);
+newRow.find('#owner').text(resource.Owner);
+return newRow;
+}
